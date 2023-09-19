5a38fe652996360594428347e27dc303f5e59790
Carlitos Programador <125935411+CarlitosNea@users.noreply.github.com>
2023-09-19 07:31:35 -0500

    actualizacion de estilos

--- Galter/Appgalter/forms.py
+++ Galter/Appgalter/forms.py
@@ -15,12 +15,68 @@ class UserForm(UserCreationForm):
             'type':'text',
             'class':'form-input',
             'placeholder':'Documento',  
-            'maxlength':'20',
+            'maxlength':'10',
             'minlength':'1'
         })
-        """ rol = forms.CharField(max_length=100)
-        imagen = forms.ImageField(required=False)
-        codigo = forms.CharField(max_length=10) """
+        self.fields['username'].widget.attrs.update({
+            'required':'',
+            'name':'username_input',
+            'id':'username_input',
+            'type':'text',
+            'class':'form-input',
+            'placeholder':'Nombre de usuario',  
+            'maxlength':'',
+            'minlength':'1'
+        })
+        self.fields['email'].widget.attrs.update({
+            'required':'',
+            'name':'email_input',
+            'id':'email_input',
+            'type':'text',
+            'class':'form-input',
+            'placeholder':'Correo electronico',  
+            'maxlength':'',
+            'minlength':'1'
+        })
+        self.fields['password1'].widget.attrs.update({
+            'required':'',
+            'name':'password1_input',
+            'id':'password1_input',
+            'type':'password',
+            'class':'form-input',
+            'placeholder':'Contraseña',  
+            'maxlength':'',
+            'minlength':'1'
+        })
+        self.fields['password2'].widget.attrs.update({
+            'required':'',
+            'name':'password2_input',
+            'id':'password2_input',
+            'type':'password',
+            'class':'form-input',
+            'placeholder':'Confirme la contraseña',  
+            'maxlength':'',
+            'minlength':'1'
+        })
+        self.fields['rol'].widget.attrs.update({
+            'required':'',
+            'name':'rol_input',
+            'id':'rol_input',
+            'type':'text',
+            'class':'form-input',
+            'placeholder':'Rol',  
+            'maxlength':'',
+            'minlength':'1'
+        })
+        self.fields['imagen'].widget.attrs.update({
+            'id':'imagen_input',
+            'type':'file',
+            'class':'form-input',
+        })
+        
+
+    rol = forms.CharField(max_length=100)
+    imagen = forms.ImageField(required=False)
     codigo = forms.CharField(max_length=10)
 
     class Meta:
@@ -50,6 +106,8 @@ class LoginForm(AuthenticationForm):
             'placeholder':'Contraseña',  
             'minlength':'1'
         })
+    username = forms.CharField()
+    password = forms.CharField()
     class Meta:
         model : User
         fields = {'username','password'}
