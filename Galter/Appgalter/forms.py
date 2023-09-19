11effec5f406d9f75bb309cfed4e90d2f2ff36c3
Carlitos Programador <125935411+CarlitosNea@users.noreply.github.com>
2023-09-19 08:46:29 -0500

    correccion de cambios

--- Galter/Appgalter/forms.py
+++ Galter/Appgalter/forms.py
@@ -16,20 +16,6 @@ class UserForm(UserCreationForm):
             'class':'controls',
             'placeholder':'Documento',  
             'maxlength':'10',
-<<<<<<< HEAD
-            'minlength':'1'
-        })
-        self.fields['username'].widget.attrs.update({
-            'required':'',
-            'name':'username_input',
-            'id':'username_input',
-            'type':'text',
-            'class':'form-input',
-            'placeholder':'Nombre de usuario',  
-            'maxlength':'',
-            'minlength':'1'
-        })
-=======
             'minlength':'1'
         })
         self.fields['username'].widget.attrs.update({
@@ -42,38 +28,10 @@ class UserForm(UserCreationForm):
             'maxlength':'20',
             'minlength':'1'
         })
->>>>>>> 3ac4fa8dd02759d35568a58a20153c3b72a53286
         self.fields['email'].widget.attrs.update({
             'required':'',
             'name':'email_input',
             'id':'email_input',
-<<<<<<< HEAD
-            'type':'text',
-            'class':'form-input',
-            'placeholder':'Correo electronico',  
-            'maxlength':'',
-            'minlength':'1'
-        })
-        self.fields['password1'].widget.attrs.update({
-            'required':'',
-            'name':'password1_input',
-            'id':'password1_input',
-            'type':'password',
-            'class':'form-input',
-            'placeholder':'Contraseña',  
-            'maxlength':'',
-            'minlength':'1'
-        })
-        self.fields['password2'].widget.attrs.update({
-            'required':'',
-            'name':'password2_input',
-            'id':'password2_input',
-            'type':'password',
-            'class':'form-input',
-            'placeholder':'Confirme la contraseña',  
-            'maxlength':'',
-            'minlength':'1'
-=======
             'type':'email',
             'class':'controls',
             'placeholder':'Correo Electronico',  
@@ -93,27 +51,12 @@ class UserForm(UserCreationForm):
             'type':'password',
             'class':'controls',
             'placeholder':'Confirmar Contraseña',  
->>>>>>> 3ac4fa8dd02759d35568a58a20153c3b72a53286
         })
         self.fields['rol'].widget.attrs.update({
             'required':'',
             'name':'rol_input',
             'id':'rol_input',
             'type':'text',
-<<<<<<< HEAD
-            'class':'form-input',
-            'placeholder':'Rol',  
-            'maxlength':'',
-            'minlength':'1'
-        })
-        self.fields['imagen'].widget.attrs.update({
-            'id':'imagen_input',
-            'type':'file',
-            'class':'form-input',
-        })
-        
-
-=======
             'class':'controls',
             'placeholder':'Rod de Usuario',  
         })
@@ -123,7 +66,6 @@ class UserForm(UserCreationForm):
             'type':'file',
             'class':'controls'
         })
->>>>>>> 3ac4fa8dd02759d35568a58a20153c3b72a53286
     rol = forms.CharField(max_length=100)
     imagen = forms.ImageField(required=False)
     codigo = forms.CharField(max_length=10)
